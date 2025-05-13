cad8d1fa9648f1b5a5f1cf3a7b75f1fe3912a69d
Lokesh Kapade <lokesh.kapade@atyeti.com>
2025-05-13 17:57:22 +0530

    Added function file and started with py library

---
+++ Fuctions_Modules/main.py
@@ -0,0 +1,9 @@
+from mymodule import add
+from module1 import greet
+from mypackage import add, substract
+
+print(add(10,4))
+print(greet("Loki"))
+
+print(add(2, 3))       
+print(substract(5, 2)) 
--- Fuctions_Modules/mymodule.py
+++ Fuctions_Modules/mymodule.py
@@ -1,5 +1,12 @@
 
-def greet(name):
-    return f"Hello, {name}!"
+def add(a, b):
+    return a + b
 
-pi = 3.14
+def multiply(a, b):
+    return a * b
+
+# Only runs when this file is executed directly
+if __name__ == "__main__":
+    print("Running tests inside math_utils module...")
+    print("Add: ", add(2, 3))          # 5
+    print("Multiply: ", multiply(2, 3)) # 6
