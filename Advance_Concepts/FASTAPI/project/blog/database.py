5460ee71a4e6b2af0ab18779bc7eab5e97b96b9e
Lokesh Kapade <lokesh.kapade@atyeti.com>
2025-10-31 10:22:17 +0530

    updated7_Fix:enviornment_DBurl_fastapi1

--- Advance_Concepts/FASTAPI/project/blog/database.py
+++ Advance_Concepts/FASTAPI/project/blog/database.py
@@ -3,13 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import psycopg2
+from dotenv import load_dotenv
 
 # SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
 # SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:loki@localhost:5432/fastapi"
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine("SQLALCHEMY_DATABASE_URL")
+engine = create_engine(SQLALCHEMY_DATABASE_URL) # type: ignore
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
